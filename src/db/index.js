9c0ab4a5f8fc7be106c9769593512a420ffdee8e
Naman Kumar <parnamnkh1@gmail.com>
2025-06-02 16:48:55 +0530

    Add MongoDB connection setup and export DB_NAME constant

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+const mongo="mongodb+srv://tubetwitt_user:tubetwitt123@cluster0.gjy0kx7.mongodb.net"
+
+const connectDB = async () =>{
+    try{
+        
+        
+        const connect_instance = await mongoose.connect(`${mongo}/${DB_NAME}`)
+       console.log(` mongo DB is connected ${connect_instance.connection.host}`);
+       
+    }
+    catch(error){
+        console.log("MOngoDB connection error",error);
+        process.exit(1)
+        
+    }
+}
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { app } from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 8001;
-app.listen(PORT, () => {
-  console.log("server is running");
-});
+connectDB()
+.then(() => {
+        app.listen(PORT, () => {
+            console.log("server is running on port ",PORT);
+          });
+          
+    })
+    .catch( (err)=>{
+        console.log("Mongodb is connection error",err);
+        
+    })

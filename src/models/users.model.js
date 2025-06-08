c023d2256f80887ce9dc34410f4138372458a7ee
Naman Kumar <parnamnkh1@gmail.com>
2025-06-08 18:17:30 +0530

    Add bcrypt and jsonwebtoken for user authentication; implement password hashing and token generation in user model; create Tweet model; integrate mongoose-aggregate-paginate in Comment and Video models.

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import { JsonWebTokenError } from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -48,4 +50,36 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 }
 )
+ 
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sing({
+        _id: this._idid,
+        email: this.email,
+        fullname: this.fullname,
+        username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,{
+       expiresIn: process.env.ACCESS_TOKEN_EXPIRE
+    }
+);
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sing({
+        _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,{
+       expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+);
+}
+
+
 export const user = mongoose.model("User", userSchema)

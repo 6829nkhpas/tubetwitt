8bb92f75119fb87a405b3c9aac40732cdd24b5a1
Naman Kumar <parnamnkh1@gmail.com>
2025-06-12 01:57:42 +0530

    Add file upload functionality to Cloudinary in utils/cloudinary.js; implement error handling and local file deletion after upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import fs from 'fs';
 
     // Configuration
     cloudinary.config({ 
@@ -7,3 +7,24 @@ import { v2 as cloudinary } from 'cloudinary';
         api_key: process.env.cloudinary_Api_key, 
         api_secret: process.env.cloudinary_Api_secret
         });
+        const uploadOnCloudinary = async (localFilePath) => {
+            try{
+                if(!localFilePath) {
+                    return Error("No file path provided");
+                }
+                const response = await cloudinary.uploader.upload(localFilePath, {
+                    resource_type: "auto", // Automatically determine the resource type
+                    
+                });
+                console.log("File uploaded successfully:", response.url);
+                fs.unlinkSync(localFilePath);
+                return response;
+
+                
+            }
+            catch(error){
+                fs.unlinkSync(localFilePath);
+                return null;
+                console.log(failed) // Delete the file if upload fails
+            }
+        }
